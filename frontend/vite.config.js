6b6c6de2012e686082bd96389bc1da38aefd2ea2
Cbih939 <memegold999@gmail.com>
2025-10-22 09:12:58 -0300

    atualização frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()], // Garante que o plugin do React (que entende JSX) está ativo
+})
