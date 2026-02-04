358187c35b4fac9dfb8da8c9a3f572a9b0f77759
Cbih939 <memegold999@gmail.com>
2026-02-04 15:44:53 -0300

    atualização rotas

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -17,7 +17,8 @@ import LoginPage from '../pages/Login.jsx';
 import NotFoundPage from '../pages/NotFound.jsx';
 import PrivacyPolicyPage from '../pages/legal/PrivacyPolicyPage.jsx'; 
 import TermsOfUsePage from '../pages/legal/TermsOfUsePage.jsx';
-
+import EsqueceuSenhaPage from './pages/EsqueceuSenha.jsx'; 
+import RedefinirSenhaPage from './pages/RedefinirSenha.jsx';
 
 // --- ADMIN ---
 import AdminDashboard from '../pages/admin/AdminDashboard.jsx';
