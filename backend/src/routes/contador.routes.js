8de7085e397a02200c2da2830ed2baf60a42213e
Cbih939 <memegold999@gmail.com>
2026-03-18 13:01:50 -0300

    Fix routes

--- backend/src/routes/contador.routes.js
+++ backend/src/routes/contador.routes.js
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import { protect, checkRole } from '../middlewares/auth.middleware.js';
 import { 
     getDashboardStats, 
-    getMyOSCs, 
+    getMyOSCs,
+    getSystemReports, 
     getRecentActivity 
 } from '../controllers/contador.controller.js';
 
