e1aecf6bb21a048a62a57e77f7b4a1d2cf8e6da4
Cbih939 <memegold999@gmail.com>
2026-09-17 12:12:48 -0300

    corrigir caminho do middleware de auth

--- backend/src/routes/report.routes.js
+++ backend/src/routes/report.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getChartData } from '../controllers/report.controller.js';
-import { verifyToken } from '../middleware/auth.js';
+import { verifyToken } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
